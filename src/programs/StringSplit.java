2bdc7799445983059d358aecefd435b5fadebf58
khanfaiz1609 <khanfaiztech1609@gmail.com>
2024-07-21 10:02:49 +0530

    .

--- src/programs/StringSplit.java
+++ src/programs/StringSplit.java
@@ -1,14 +1,11 @@
 package programs;
 
 public class StringSplit {
-
 	public static void main(String[] args) {
-		String str="My Name Is Faiz";
-		String[] str1=str.split(" ");
+		String str = "My Name Is Faiz";
+		String[] str1 = str.split(" ");
 		for (int i = 0; i < str1.length; i++) {
 			System.out.println(str1[i]);
 		}
-		
 	}
-
 }
